2d7917663e50382ec006e5f94705ff17c1b11906
Sahaj Garg <sahaj.garg@mtxb2b.com>
2020-11-08 23:40:54 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function handleClick(){
 	var side_element = document.getElementById("side-bar");
 	side_element.classList.toggle("slds-show_small");
-	side_element.style.background = "rgb(48, 48, 48)";
 	document.getElementById("side-bar").style.width = "80%";
 	var main_element = document.getElementById("main-bar");
 	main_element.classList.toggle("slds-show_small");
