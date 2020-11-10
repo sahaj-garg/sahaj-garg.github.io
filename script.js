4f615a23bd2ef2ae434cfab432efeb6d435297e1
Sahaj Garg <sahaj.garg@mtxb2b.com>
2020-11-10 23:30:29 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-function handleClick(){
+function openSidebar(){
 	var side_element = document.getElementById("side-bar");
-	side_element.classList.toggle("slds-show_small");
-	document.getElementById("side-bar").style.width = "80%";
-	var main_element = document.getElementById("main-bar");
-	main_element.classList.toggle("slds-show_small");
+	side_element.classList.remove("slds-show_small");
+	var hamburger = document.getElementById('hamburger');
+	var close_menu = document.getElementById('close-menu');
+	hamburger.classList.add('slds-hide');
+	close_menu.classList.remove('slds-hide');
+	close_menu.classList.add('slds-hide_small');
+}
+
+function closeSidebar(){
+	var side_element = document.getElementById("side-bar");
+	side_element.classList.add("slds-show_small");
+	var hamburger = document.getElementById('hamburger');
+	var close_menu = document.getElementById('close-menu');
+	hamburger.classList.remove('slds-hide');
+	close_menu.classList.add('slds-hide');
+	close_menu.classList.remove('slds-hide_small');
+}
+
+function show_card_view(){
+	
 }
